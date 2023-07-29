e7314e002c1468fc358cdf6dec2a4135005b2f3a
Thiago Tranquilim <trtranquilim@gmail.com>
2023-07-29 14:14:36 -0300

    Commit Controle de Fluxo

---
+++ ControleDeFluxo/src/ExemploForArray.java
@@ -0,0 +1,10 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String nomes [] = {"Thiago", "Leonardo", "Giovani", "Higor"};
+
+        for (String nome : nomes){
+            System.out.println(nome);
+        }
+
+    }
+}
